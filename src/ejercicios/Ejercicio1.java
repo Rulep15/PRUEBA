a3d71630d5fa6aa21e23cb4b3cc4ad71dfc39025
Rulep15 <lucasrulep@gmail.com>
2023-07-17 22:40:54 -0400

    FINAL

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,106 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        //Un cliente compra una moto y se percata que si lo compraba el dia martes tiene un descuento del 12% luego si lo compra el dia sabado tiene un decuento del 18% y si es feriado un 25
+
+        //DEFINICION
+        int monto;
+        char continuar;
+        
+
+        System.out.println("Monto a pagar por la moto");
+        System.out.println("Ingrese monto de la compras");
+        
+            
+            
+    }
+
+}
+// Definir variables
+/*Definir monto Como Entero
+
+Definir continuar Como Caracter
+
+continuar <- 's'
+
+// Ingresar datos
+Escribir '****Monto a pagar por la moto****'
+
+Escribir 'Ingrese monto de la compra : '
+
+Leer monto
+
+Escribir '****Averigue si Ud. tiene un descuento****'
+
+Repetir
+
+ Escribir '1.- Hoy es martes'
+
+ Escribir '2.- Hoy es sabado'
+
+ Escribir '3.- Hoy es un día feriado'
+
+ Escribir '4.- Ninguna de las anteriores '
+
+ Escribir 'Ingrese respuesta (1-4) '
+
+ Repetir
+
+  Leer respuesta
+
+ Mientras Que (respuesta<>'1') Y (respuesta<>'2') Y (respuesta<>'3') Y (respuesta<>'4')
+
+ // Procesar datos
+ Segun respuesta  Hacer
+
+  '1':
+
+   Escribir 'Descuento del 12%   '
+
+   descuento <- descuento+(monto*0.12)
+
+  '2':
+
+   Escribir 'Descuento del 18%   '
+
+   descuento <- descuento+(monto*0.18)
+
+  '3':
+
+   Escribir 'Descuento del 25%   '
+
+   descuento <- descuento+(monto*0.12)
+
+  '4':
+
+   Escribir 'No tiene descuento  '
+
+   descuento <- 0
+
+ FinSegun
+
+ Escribir 'Resultados:  '
+
+ // mostrar resultado en pantalla
+ Escribir '...........................'
+
+ Escribir 'Su descuento es: ',descuento
+
+ Escribir 'Nuevo monto a pagar: ',monto-descuento
+
+ Repetir
+
+  Escribir 'Continuar con otra factura?: (s/n)'
+
+  Leer continuar
+
+ Hasta Que (continuar=='s' O continuar=='n')
+
+Hasta Que continuar=='n'
+
+FinAlgoritmo*/
+
